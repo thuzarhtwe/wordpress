7e8df87f4d7d39d47868b2ae3d4eb3a11858db89
thuzarhtew <scm.thuzarhtwe@gmail.com>
2018-08-21 16:26:06 -0700

    JSON

--- gulpfile.js
+++ gulpfile.js
@@ -2,89 +2,83 @@
 /*------------------------------------------------------------------------------
  * 1. DEPENDENCIES
 ------------------------------------------------------------------------------*/
-var gulp        = require('gulp'), // gulp
-    $           = require('gulp-load-plugins')({ pattern: ['gulp-*', 'gulp.*'] }), // load plugins
-    runSequence = require('run-sequence'), // run order
-    rimraf      = require('rimraf'), // delete files/folder directly
-    browserSync = require('browser-sync'), // for browser syncronization
-    reload      = browserSync.reload // reload
-;
+var gulp = require('gulp'),
+  $g = require('gulp-load-plugins')({ pattern: ['gulp-*', 'gulp.*'] }),
+  runSequence = require('run-sequence'),
+  rimraf = require('rimraf'),
+  sourcemaps = require('gulp-sourcemaps')
+  ;
 
 /*------------------------------------------------------------------------------
  * 2. FILE DESTINATIONS (RELATIVE TO ASSSETS FOLDER)
 ------------------------------------------------------------------------------*/
 // Declare configuration
 var conf = {
-	src   :	"./src",
-	dist  :	"./assets",
-	url   :	"localhost:8888",
-	imgExt: "png,jpg,jpeg,gif,svg"
+  dist_js: "./assets",
+  dist_css: "./style.css",
+  dist_csscomb: './csscomb.json',
+  dist_scss: "./src/scss/**/*.scss",
+  dist_watchcss: "['./inc/slider/css/*.css', './layouts/*.css']",
+  dist_watchjs: "./js/*.js",
+  dist_php: "['./*.php', './template-parts/*.php', './inc/**/*.php']"
+
 };
 
 // Declare plumberErrorHandler (used for gulp error)
 var plumberErrorHandler = {
-	errorHandler: $.notify.onError({
-		title: 'Gulp',
-		message: 'Error: <%= error.message %>'
-	})
+  errorHandler: $g.notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+  })
 };
 
 /*------------------------------------------------------------------------------
  * 3. Gulp Tasks
 ------------------------------------------------------------------------------*/
-gulp.task('default', ['build', 'watch', 'browser-sync']);
+gulp.task('default', ['build', 'watch']);
 
-gulp.task('clean', function(cb) {
-	rimraf(conf.dist, cb);
+gulp.task('build', ['clean'], function (cb) {
+  runSequence(['js', 'sass', 'css', 'php'], cb);
 });
 
-gulp.task('build', ['clean'], function(cb) {
-	runSequence(['js', 'sass', 'css', 'php'], cb);
+gulp.task('watch', function () {
+  gulp.watch(conf.dist_watchjs, ['js']);
+  gulp.watch(conf.dist_scss, ['sass']);
+  gulp.watch(conf.dist_watchcss, ['css']);
+  gulp.watch(conf.dist_php, ['php']);
 });
 
-gulp.task('browser-sync', function () {
-	browserSync({
-		proxy: conf.url
-	});
-});
-
-gulp.task('watch', function() {
-	gulp.watch(conf.src + '/scss/**/*.scss', ['sass']);
-	gulp.watch(['./inc/slider/css/*.css', './layouts/*.css'], ['css']);
-	gulp.watch([conf.src + '/js/*.js', './js/*.js'], ['js']);
-	gulp.watch(['./*.php', './template-parts/*.php', './inc/**/*.php'], ['php']);
-});
-
-gulp.task('php', function() {
-	gulp.src(['./*.php', './template-parts/*.php', './inc/**/*.php'])
-		.pipe($.plumber(plumberErrorHandler))
-		.pipe(reload({ stream: true }));
-	}
-);
-
-gulp.task('sass', function () {
-	gulp.src(conf.src + '/scss/*.scss')
-		.pipe($.plumber(plumberErrorHandler))
-		.pipe($.sourcemaps.init())
-		.pipe($.sass())
-		.pipe($.autoprefixer())
-		.pipe($.sourcemaps.write())
-		.pipe(gulp.dest('./'))
-		.pipe(reload({ stream: true }));
-});
-
-gulp.task('css', function () {
-	gulp.src(['./inc/slider/css/*.css', './layouts/*.css'])
-  	.pipe($.plumber(plumberErrorHandler))
-  	.pipe(reload({ stream: true }));
+gulp.task('clean', function () {
+  rimraf.sync(conf.dist_js);
+  rimraf.sync(conf.dist_css);
 });
 
 gulp.task('js', function () {
-	gulp.src([conf.src + '/js/*.js'])
-  	.pipe($.plumber(plumberErrorHandler))
-  	.pipe($.concat('bundle.js'))
-  	.pipe($.uglify({ preserveComments: 'license' }))
-  	.pipe($.rename({ suffix: '.min' }))
-  	.pipe(gulp.dest(conf.dist + '/script/'))
-  	.pipe(reload({ stream: true }));
+  gulp.src(conf.dist_watchjs)
+    .pipe($g.plumber(plumberErrorHandler))
+    .pipe($g.concat('bundle.js'))
+    .pipe($g.uglify())
+    .pipe($g.rename({ suffix: '.min' }))
+    .pipe(gulp.dest(conf.dist_js + '/script/'));
+});
+
+gulp.task('sass', function () {
+  gulp.src(conf.dist_scss)
+    .pipe($g.plumber(plumberErrorHandler))
+    .pipe($g.sourcemaps.init())
+    .pipe($g.sass())
+    .pipe($g.autoprefixer())
+    .pipe($g.sourcemaps.write())
+    .pipe($g.csscomb(conf.dist_csscomb))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('css', function () {
+  gulp.src(conf.dist_watchcss)
+    .pipe($g.plumber(plumberErrorHandler));
+});
+
+gulp.task('php', function () {
+  gulp.src(conf.dist_php)
+    .pipe($g.plumber(plumberErrorHandler));
 });
